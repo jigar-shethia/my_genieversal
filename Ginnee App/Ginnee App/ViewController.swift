cae3435196ffa6726342f95b1cf4d4c14ff9dfc7
jigar-shethia <30865218+jigar-shethia@users.noreply.github.com>
2022-01-02 02:50:01 +0530

    Add files via upload
    First commit

---
+++ Ginnee App/Ginnee App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ginnee App
+//
+//  Created by Jigar shethia on 02/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
