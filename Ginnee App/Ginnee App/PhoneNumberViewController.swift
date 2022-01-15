bee4cb8cde9097fa95edbf75a6b7fad44ced53d9
Jigar Shethia <jigar4444@gmail.com>
2022-01-15 19:25:41 +0530

    Added email login screen.

---
+++ Ginnee App/Ginnee App/PhoneNumberViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PhoneNumberViewController.swift
+//  Ginnee App
+//
+//  Created by Jigar shethia on 11/01/22.
+//
+
+import UIKit
+class PhoneNumberViewController: UIViewController {
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func proceedClicked(_ sender: UIButton) {
+        
+        // Do valiadation
+        
+        // Send to OTP scree.
+        
+    }
+}
